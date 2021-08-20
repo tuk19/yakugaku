6f1b9db5f749e7e4cc58e1726513d5d378f29fb9
tuk19 <wakhome@t-waknoMacBook-Pro.local>
2021-08-20 13:06:28 +0900

    fix: questions_controllerのindex new createを追加

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,21 @@
 class QuestionsController < ApplicationController
   def index
+    @questions = Question.all
   end
 
   def new
+    @question = Question.new
+    @answer = @question.answers.build
   end
 
   def create
+    @question = Question.new(question_params)
+    if @question.save
+      flash[:notice] = "スケジュールを登録しました"
+      redirect_to :question
+    else
+      render "new"
+    end
   end
 
   def show
@@ -19,4 +29,9 @@ class QuestionsController < ApplicationController
 
   def destroy
   end
+
+  private
+  def question_params
+      params.require(:question).permit(:number, :difficulty, :statement, :one_answer, answers_attributes: [:answer, :correct])
+    end
 end
