ae155752f7d966e6bc7d11a8b5ddd6fa1bb1584b
tuk19 <wakhome@t-waknoMacBook-Pro.local>
2021-08-21 18:34:24 +0900

    fix: new createにformオブジェクトを追加、 showを作成

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,21 +4,27 @@ class QuestionsController < ApplicationController
   end
 
   def new
-    @question = Question.new
-    @answer = @question.answers.build
+    @register_questions_form = RegisterQuestionsForm.new
+    if Question.all.count == 0
+      @number = 0
+    else
+      @number = Question.all.count
+    end
   end
 
   def create
-    @question = Question.new(question_params)
-    if @question.save
+    @register_questions_form = RegisterQuestionsForm.new(question_params)
+    if @register_questions_form.save
       flash[:notice] = "スケジュールを登録しました"
-      redirect_to :question
+      redirect_to questions_path
     else
       render "new"
     end
   end
 
   def show
+    @question = Question.find(params[:id])
+    @answers = @question.answers
   end
 
   def edit
@@ -32,6 +38,7 @@ class QuestionsController < ApplicationController
 
   private
   def question_params
-      params.require(:question).permit(:number, :difficulty, :statement, :one_answer, answers_attributes: [:answer, :correct])
+      params.require(:register_questions_form).permit(:number, :difficulty, :statement, :one_answer,
+        :answer1, :correct1, :answer2, :correct2, :answer3, :correct3, :answer4, :correct4)
     end
 end
